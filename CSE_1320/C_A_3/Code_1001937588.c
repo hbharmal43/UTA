687b0dd78791bd16a53d0f6dddb63f78a8280fe3
unknown <hasnainbharmal4@gmail.com>
2022-03-09 19:05:13 -0600

    Done 30%

--- CSE_1320/C_A_3/Code_1001937588.c
+++ CSE_1320/C_A_3/Code_1001937588.c
@@ -1,22 +1,58 @@
 // Name:Hasnain Bharmal
-//ID; 1001937588
+// ID; 1001937588
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-void fillBingoCard(int temp);
+
+int fillBingoCard(int temp[5][5]);
 int main()
 {
-int bingo_card[4][4];
-fillBingoCard(bingo_card);
+    int bingo_card[5][5];
+        srand(time(0));
+
+    fillBingoCard(bingo_card);
+
+    for (int i = 0; i < 5; i++){
+        for(int j=0;j<5;j++){
+        printf("%d\t", bingo_card[j][i]);
+    }
+printf("\n");
 }
 
-void fillBingoCard(int temp[4][4])
+    return 0;
+}
+
+int fillBingoCard(int temp[5][5])
 {
-srand(time(0));
-for(int i=0;i<5;i++){
-    temp[0][i]=rand() % 15+1;
-    printf("%d",temp[0][i]);
-}
+    for (int i = 0; i < 5; i++)
+    {
+        temp[i][0] = rand() % 15 + 1;
+        //printf("%d\n", temp[0][i]);
+    }
 
+    for (int j = 0; j < 5; j++)
+    {
+        temp[j][1] = rand() % 15 + 16;
+        //printf("%d\t", temp[0][j]);
+    }
+    printf("\n");
+    for (int i = 0; i < 5; i++)
+    {
+        temp[i][2] = rand() % 15 + 31;
+        //printf("%d\t", temp[0][i]);
+    } 
+
+    for (int i = 0; i < 5; i++)
+    {
+        temp[i][3] =rand() % 15 + 46;
+        //printf("%d\t", temp[0][i]);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        temp[i][4] = rand() % 15 + 61;
+       // printf("%d\t\n", temp[0][i]);
+    }
 }
 
